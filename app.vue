7b8133b64a2d085ad2b1e3ee42fa917e35a129a2
alfianchii <pianaee379@gmail.com>
2024-05-27 10:17:16 +0700

    feat: setup prettier

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+const { txt } = defineProps<{
+    txt: string;
+}>();
+
+const ok = {
+    nama: 'Alfian',
+    nami: 'Alfian',
+    namu: 'Alfian',
+    name: 'Alfian',
+    namo: 'Alfian',
+    namaa: 'Alfian',
+    namaaa: 'Alfian',
+    nam: 'Alfian',
+};
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <span
+            class="mx-1 bg-blue-500"
+            v-text="`dw`"
+            :class="txt"
+            text-ok
+            flex
+            border
+            px1
+            py2
+        ></span>
+    </div>
 </template>
